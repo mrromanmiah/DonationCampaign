730ab8092013d481c4042826d353e1a912945786
mrromanmiah <mrromanmiah@gmail.com>
2023-09-25 11:19:17 +0600

    router setup completed

---
+++ src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+    );
+};
+
+export default Navbar;

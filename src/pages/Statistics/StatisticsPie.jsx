31ecfdd2de050e50a6814b349cb4014b3ec795ae
mrromanmiah <mrromanmiah@gmail.com>
2023-09-27 02:14:58 +0600

    pieChart creation completed. now search function and making responsive are  remaining

--- src/pages/Statistics/StatisticsPie.jsx
+++ src/pages/Statistics/StatisticsPie.jsx
@@ -1,67 +1,39 @@
 
-import { PieChart, Pie, Cell } from "recharts";
-import React, { useEffect } from 'react';
+import { useEffect, useState } from "react";
+import { Chart } from "react-google-charts";
 
+const StatisticsPie = () => {
 
+    const [donation, setDonation] = useState([])
 
-const StatisticsPie = ({ card }) => {
+    useEffect(() => {
+        const donationItems = JSON.parse(localStorage.getItem('donation'));
+    setDonation(donationItems);
+    
 
-    const donationItems = JSON.parse(localStorage.getItem('donation'));
+    }, []);
+    const yourDonation = donation.length;
+    const totalDonation = 12 - yourDonation
+    
 
+    const data = [
+        ["Task", "Hours per Day"],
+        ["Your Donation", yourDonation],
+        ["Total Donation", totalDonation]
+    ];
 
-
-    // const data = [
-    //     { name: "Group A", value: 12 },
-    //     { name: "Group B", value: 4 }
-    // ];
-
-    const COLORS = ["#FF444A", "#00C49F"];
-
-    const RADIAN = Math.PI / 180;
-    const renderCustomizedLabel = ({
-        cx,
-        cy,
-        midAngle,
-        innerRadius,
-        outerRadius,
-        percent,
-        index
-    }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-        return (
-            <text
-                x={x}
-                y={y}
-                fill="white"
-                textAnchor={x > cx ? "start" : "end"}
-                dominantBaseline="central"
-            >
-                {`${(percent * 100).toFixed(0)}%`}
-            </text>
-        );
+    const options = {
+        title: "My Daily Activities",
     };
+
     return (
-        <div>
-            <PieChart width={400} height={400}>
-                <Pie
-                    data={data}
-                    cx={200}
-                    cy={200}
-                    labelLine={false}
-                    label={renderCustomizedLabel}
-                    outerRadius={80}
-                    fill="#8884d8"
-                    dataKey="value"
-                >
-                    {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                    ))}
-                </Pie>
-            </PieChart>
-        </div>
+        <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"100%"}
+            height={"400px"}
+        />
     );
 };
 

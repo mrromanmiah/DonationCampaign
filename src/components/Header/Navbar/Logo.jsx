c6621eba63db1bb65e52f79fe7e661858ea17da0
mrromanmiah <mrromanmiah@gmail.com>
2023-09-27 23:02:25 +0600

    logo icon was not working. now it is fixed

--- src/components/Header/Navbar/Logo.jsx
+++ src/components/Header/Navbar/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
     return (
         <div className="flex items-center gap-5">
-            <img src="/src/assets/favicon.png" alt="Logo" />
+            <img src={"https://i.ibb.co/nMsBCBV/logo.png"} alt="Logo" />
             <div>
                 <h1 className="text-[#FF444A] text-4xl font-bold">Donation</h1>
                 <h2 className="text-[#0b0b0bb3] text-lg font-medium tracking-[9.54px]">Campaign</h2>
